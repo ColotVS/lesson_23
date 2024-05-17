c21050ddc78ab899a30b09924e7a03e31c9ac4c1
Vladislav Kolotilov <kolotilov98@mail.ru>
2024-05-17 12:28:57 +0700

    Интерфейсы. Практика 5 Урок 23, задача 6

---
+++ lesson_06.java
@@ -0,0 +1,28 @@
+public class lesson_06 {
+    /**
+     * Исправь 4 ошибки в программе, чтобы она компилировалась.
+     */
+    public static void main(String[] args) {
+
+        System.out.println(Dream.HOBBY.toString());
+        System.out.println(new Hobby().toString());
+
+    }
+
+    interface Desire {
+    }
+
+    interface Dream {
+        Hobby HOBBY = new Hobby();
+    }
+
+    static class Hobby implements Dream, Desire {
+        static int INDEX = 1;
+
+        @Override
+        public String toString() {
+            INDEX++;
+            return "" + INDEX;
+        }
+    }
+}
